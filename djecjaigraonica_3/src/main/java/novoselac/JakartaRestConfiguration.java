e8d15a7209ed3cdc288e314d7161ff8e7c2960a5
Martina Novoselac <novoselac.martina@gmail.com>
2023-07-09 17:28:18 +0200

    ispravak u pom-u , sve radi

--- djecjaigraonica_3/src/main/java/novoselac/JakartaRestConfiguration.java
+++ djecjaigraonica_3/src/main/java/novoselac/JakartaRestConfiguration.java
@@ -1,7 +1,8 @@
-package novoselac.djecjaigraonica_3;
+package novoselac;
 
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 /**
  * Configures Jakarta RESTful Web Services for the application.
