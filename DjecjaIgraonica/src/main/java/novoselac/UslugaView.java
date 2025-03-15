4bb5e3b3f693c9f2f3cbd24d50a84452a1b3bbc3
Martina Novoselac <novoselac.martina@gmail.com>
2025-03-15 01:26:56 +0100

    izmjena css-a

--- DjecjaIgraonica/src/main/java/novoselac/UslugaView.java
+++ DjecjaIgraonica/src/main/java/novoselac/UslugaView.java
@@ -16,17 +16,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  *
  * @author Administrator
  */
-
-
 @WebServlet("/UslugaView")
-public class UslugaView extends HttpServlet{
-    
+public class UslugaView extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -41,173 +39,128 @@ public class UslugaView extends HttpServlet{
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet javaServlet</title>");            
+            out.println("<title>Servlet UslugaView</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Pozdrav da vidimo radi li!!! </h1>");
+            out.println("<h1>Pozdrav, ovo je UslugaView servlet!</h1>");
             out.println("</body>");
             out.println("</html>");
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         response.setContentType("text/html");
-        PrintWriter out= response.getWriter();
-       
-         
-        //OBRAZAC ZA POPUNJAVANJE TABLICE USLUGA
+        PrintWriter out = response.getWriter();
 
-        
-        out.println("<div class=\"index-intro\"> \n" +
-"		<div class=\"wrapper\">\n" +
-"			<h1>Unos usluga</h1>\n" +
-"		<form action=\"JavaServlet\" method =\"post\">\n" +
-"                            \n" +
-"                                 <!--dodavanje tablice-->\n" +
-"                         \n" +
-"                   \n" +
-"                   \n" +
-"                   <!--</textarea>-->\n" +
-"                   <br><br>\n" +
-"                <div>\n" +
-"                   <input type=\"naziv\" id=\"naziv\" name= \"naziv\" placeholder='Naziv'>\n" +
-"                   <br><br>\n" +
-"                </div>\n" +
-"       \n" +
-"                <div>\n" +
-"                   <input type=\"jedinicaMjere\" id=\"jedinicaMjere\" name= \"jedinicaMjere\" placeholder='Jedinica mjere'>\n" +
-"                   <br><br>\n" +
-"                </div>\n" +
-"       \n" +
-"                <div>\n" +
-"                   <input type=\"cijena\" id=\"cijena\" name= \"cijena\" placeholder='Cijena'>\n" +
-"                   <br><br>\n" +
-"                </div>\n" +
-"               \n" +
-"                            \n" +
-"                 <div>\n" +
-"                   <input type=\"kolicina\" id=\"kolicina\" name= \"kolicina\" placeholder='Količina'>\n" +
-"                   <br><br>\n" +
-"                </div>\n" +
-"                   ");
-        
-                  out.println("<button type=\"submit\" class=\"btn btn-primary\">Dodaj</button>");
-        
-        
-//TABLICA
-       //jdbc connection
+        // Generiranje HTML-a
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Usluga View</title>");
+        //out.println("<link rel='stylesheet' type='text/css' href='style.css'>"); // Dodajte vaš CSS
+        out.println("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'>"); // Dodajte Font Awesome
+        out.println("</head>");
+        out.println("<body>");
 
-       	try {
-         //2b
-	Class.forName("com.mysql.cj.jdbc.Driver");
-	//3
-        Connection con = DriverManager.getConnection//jdbc:mysql://localhost/djecjaigraonicahib
-	("jdbc:mysql://localhost/djecjaigraonicahib", "root", "");
-	
-        
-        String sql;
-        
-        sql= "select * from  usluga";
-        Statement stmt = con.createStatement();
-        ResultSet rs;
-        rs=stmt.executeQuery(sql);
-        
-        
- 
-        
-       
-        
-             
-        
-        out.println("<table cellspacing='0' width='350px' border='1'>");
-        out.println("<tr>");
-        out.println("<td> Cijena</td>");
-        out.println("<td> Jedinica mjere</td>");
-        out.println("<td> Količina</td>");
-        out.println("<td> Naziv</td>");
-        out.println("<td> Uredi</td>");
-        out.println("<td> Briši</td>");
-        
-        out.println("</tr>");
-        
-       
-       
-        
-        while (rs.next()) {
+        // Obrazac za unos usluga
+        out.println("<div class='index-intro'>");
+        out.println("<div class='wrapper'>");
+        out.println("<h1>Unos usluga</h1>");
+        out.println("<form action='JavaServlet' method='post'>");
+        out.println("<div>");
+        out.println("<input type='text' id='naziv' name='naziv' placeholder='Naziv'>");
+        out.println("<br><br>");
+        out.println("</div>");
+        out.println("<div>");
+        out.println("<input type='text' id='jedinicaMjere' name='jedinicaMjere' placeholder='Jedinica mjere'>");
+        out.println("<br><br>");
+        out.println("</div>");
+        out.println("<div>");
+        out.println("<input type='text' id='cijena' name='cijena' placeholder='Cijena'>");
+        out.println("<br><br>");
+        out.println("</div>");
+        out.println("<div>");
+        out.println("<input type='text' id='kolicina' name='kolicina' placeholder='Količina'>");
+        out.println("<br><br>");
+        out.println("</div>");
+        out.println("<button type='submit' class='btn btn-primary'>Dodaj</button>");
+        out.println("</form>");
+        out.println("</div>");
+        out.println("</div>");
+
+        // Tablica za prikaz usluga
+        try {
+            // Učitavanje JDBC drivera
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Povezivanje s bazom podataka
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/djecjaigraonicahib", "root", "");
+
+            // SQL upit za dohvat podataka
+            String sql = "SELECT * FROM usluga";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // Generiranje tablice
+            out.println("<table cellspacing='0' width='400px' border='1'>");
             out.println("<tr>");
-            out.println("<td>" + rs.getString("cijena")+"</td>" );
-            out.println("<td>" + rs.getString("jedinicaMjere")+"</td>" );
-            out.println("<td>" + rs.getString("kolicina")+"</td>");
-            out.println("<td>" + rs.getString("naziv")+"</td>");
-           
-            out.println("<td>"  + "<a href='VratiUslugu?naziv=" +  rs.getString("naziv")  + "'> Uredi </a>" + "</td>");
-            out.println("<td>"  + "<a href='BrisiUslugu?naziv=" +  rs.getString("naziv")  + "'> Briši </a>" + "</td>");
-          
-       out.println("</tr>");
+            out.println("<th>Cijena</th>");
+            out.println("<th>Jedinica mjere</th>");
+            out.println("<th>Količina</th>");
+            out.println("<th>Naziv</th>");
+            out.println("<th>Uredi</th>");
+            out.println("<th>Briši</th>");
+            out.println("</tr>");
+
+            // Popunjavanje tablice podacima iz baze
+            while (rs.next()) {
+                out.println("<tr>");
+                out.println("<td>" + rs.getString("cijena") + "</td>");
+                out.println("<td>" + rs.getString("jedinicaMjere") + "</td>");
+                out.println("<td>" + rs.getString("kolicina") + "</td>");
+                out.println("<td>" + rs.getString("naziv") + "</td>");
+                out.println("<td><a href='VratiUslugu?naziv=" + rs.getString("naziv") + "'><i class='fas fa-edit'></i> Uredi</a></td>");
+                out.println("<td><a href='BrisiUslugu?naziv=" + rs.getString("naziv") + "'><i class='fas fa-trash'></i> Briši</a></td>");
+                out.println("</tr>");
+            }
+
+            out.println("</table>");
+
+            // Link za povratak na glavni izbornik
+            out.println("<br><br>");
+            out.println("<a href='index.html'><i class='fas fa-home'></i> Vrati se nazad na glavni izbornik</a>");
+
+            // Zatvaranje resursa
+            rs.close();
+            stmt.close();
+            con.close();
+
+        } catch (ClassNotFoundException ex) {
+            out.println("<font color='red'>Greška: Nije pronađen JDBC driver.</font>");
+            Logger.getLogger(UslugaView.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            out.println("<font color='red'>Greška: Problem s bazom podataka.</font>");
+            Logger.getLogger(UslugaView.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        out.println("<table>");
-        
- out.println("<a href=index.html>vrati se nazad na glavni izbornik</a>");
-        
-   
-        
-        
 
+        out.println("</body>");
+        out.println("</html>");
+    }
 
-		} catch (ClassNotFoundException ex) {
-              Logger.getLogger(novoselac.model.Usluga.class.getName());
-			out.println(ex);
-
-         
-		}
-	catch(SQLException ex){
-            out.println("<font color= 'red'> Record Failed </fornt>");
-        }
-        
-	
-  }
-    
-  
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      doGet(request, response);
+        doGet(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        return "Servlet za prikaz i upravljanje uslugama.";
+    }
 }
-
